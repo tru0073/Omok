faae425d72e430c806da01901d6929dc76bcbb5b
Rita Truong <rita.ph.truong@gmail.com>
2023-05-11 04:49:08 +1000

    Created rules factories

--- script.js
+++ script.js
@@ -59,9 +59,83 @@ const Player = (piece) => {
 
  })();
 
-// Widget logic
+////TODO: Rules factory
+ const rules = () => {};
+
+//TODO: Make normal class
+//// Pre-move Rules
+const preMoveRules = (() => {
+
+    // TODO: Need to make parent to inherit from for the next 4 lines to avoid repeatable code
+    const lowerBound = 0;
+    const upperBound = 14;
+    const piece = boardGame.getState(position);   // Get the piece at this position
+    let rulesList = [];
+
+    // Check if the move will pass 
+    const checkPassesRules = (position) => {
+        // Iterate through every rule
+        for(rule in rulesList){
+            if(rule(position)==False){ //TODO????surely can write this better
+                return False;
+            };
+            return True;
+        };
+    };
+
+    // Can't put a piece where there is already a piece
+    const checkTakenRule = (position) => {
+        if(piece == ""){
+            return True;
+        } else {
+            return False
+        };
+    };
+    rulesList.push(checkTakenRule);
+
+    //TODO: Double-3 rule
+
+})();
+
+//// Winning rules
+const winConditionRules = (() => {
+    // TODO: Need to make parent to inherit from for the next 4 lines to avoid repeatable code
+    const lowerBound = 0;
+    const upperBound = 14;
+    const piece = boardGame.getState(position);
+    let rulesList = [];
+
+    // Check if a win rule has been met
+
+    // Vertical win condition
+    // Horizontal win condition
+    // Negative diagonal win condition
+    // Positive diagonal win condition
+
+})();
 
 //// Game logic
-// Placing a piece down - check for broken rules
-// Rule: Can't place a piece where there is already a piece
-// Winning condition: 5 in a row
+const gameController = (() => {
+    const player1 = Player("x");
+    const player2 = Player("y");
+    let currPlayer = player1;
+
+    // Place a piece down
+    const makeMove = (currPlayer, index) => {
+        // Check //TODO: Is it a legitimate move?
+
+        // Has the winning condition been met?
+    };
+
+        // 
+    // New game
+
+    // End the game
+
+})();
+
+
+
+//// Widget logic
+
+// Place a piece
