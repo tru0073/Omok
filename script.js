8f8333b68ab819f7c9ddb60cd54709407814f521
Rita Truong <rita.ph.truong@gmail.com>
2023-05-26 03:44:03 +1000

    Added DOM logic

--- script.js
+++ script.js
@@ -1,219 +1,97 @@
-// Construct global variables 
-var boardSize = 15;
-
-//// Player
+// Player
 const Player = (piece) => {
-    this.piece = piece;
+    this.piece = piece; 
 
     const getPiece = () => {
-        return this.piece;
+        return piece;
     }
-    return getPiece();
+
+    return {getPiece};
 };
 
 //// Gameboard
-// These are functions related to the gameboard
- const gameBoard = (() => {
+const gameBoard = (() => {
+    let lastPiecePlayed = "X";
 
-    let board = ["", "", "", "", "", "", "", "", "", "","", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+    const board = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
 
-    // Create an empty game board in the event of a new game
-    const createNewBoard = () => {
-         board = ["", "", "", "", "", "", "", "", "", "","", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-         "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    const setState = (position, piece) => {
+        board[position] = piece;
+        lastPiecePlayed = piece;
     };
 
-    // Get the state of a position on the board
-    const getState = (index) => {
-        return board[index];
+    const getState = (position) => {
+        return board[position];
     };
 
-    // Change the state of a position on the board
-    const setState = (index, piece) => {
-        board[index] = piece;
-    };
-
-    return {getState, setState};
-
- })();
-
-//// Rules factory
-const rules = ((position, piece) => {
-    const lowerBound = 0;
-    const upperBound = boardSize*boardSize - 1;
-
-    const state = boardGame.getState(position);   // Get the piece at this position
-
-    // Can't put a piece where there is already a piece
-    const freeSpaceRule = (position, piece) => {
-            if(state == ""){
-                return True;
-            } else {
-                return False
-            };
+    const resetBoard = () => {
+        for(let i=0; i < board.length; i++){
+            board[i] = "";
         };
+    };
 
-    //TODO: Double-3 rule
+    const getLastPiece = () => {
+        return lastPiecePlayed;
+    }
 
-    //// Winning condition! 5 in a row - no less and no more
-    // Vertical win condition
-
-    return {freeSpaceRule};
-
+    return {setState, getState, resetBoard, getLastPiece};
 })();
 
-//TODO: Lots of repeated code here ....
-//// Winning rules
-const winConditionRules = ((position, piece) => {
-
-    // Check if a win rule has been met for a piece x and at position y
-
-    // Vertical win condition
-    const verticalWin = () => {
-
-        // Check upwards (-15)
-        let chainPosition = position;
-        let chainLength = 0;
-        let currentPiece = piece;
-        while (chainPosition > 0 && gameBoard.getState(chainPosition) == piece){
-            chainLength += 1
-            chainPosition -= 15;
-        };
-
-        // Check downwards (+15)
-        chainPosition = position;   // Reset variables
-        currentPiece = piece;
-        while (chainPosition < boardSize * boardSize - 1 && gameBoard.getState(chainPosition) == piece) {
-            chainLength += 1
-            chainPosition += 15;
-        };
-
-        return chainLength - 1;         // -1 to compensate for counting the last-placed piece twice
-    };
-
-    // Horizontal win condition
-    const horizontalWin = () => {
-        let chainPosition = position;
-        let chainLength = 0;
-
-        // Check left (-1)
-        while (chainPosition < 225 && chainPosition % 15 != 0 && gameBoard.getState(chainPosition) == piece){
-            chainLength += 1;
-            chainPosition -= 1;
-        };
-
-        // Check right (+1)
-        chainPosition = position;
-        currentPiece = piece;
-        while (chainPosition < 225 && (chainPosition + 1) % 15 != 0 && currentPiece == piece){
-            chainLength += 1;
-            chainPosition -= 1;
-            currentPiece = gameBoard.getState(chainPosition);
-        };
-    };
-
-    // Negative diagonal win condition
-    const negDiagWin = () => {
-        let chainPosition = position;
-        let chainLength = 0;
-
-        // Check upwards (-16)
-        while (chainPosition > 0 && (chainPosition) % 15 != 0 && gameBoard.getState(chainPosition) == piece){
-            chainLength += 1;
-            chainPosition -= 16;
-        };
-
-        // Check downwards (+16)
-        chainPosition = position;
-        currentPiece = piece;
-        while (chainPosition < 225 && (chainPosition + 1) % 15 != 0 && currentPiece == piece){
-            chainLength += 1;
-            chainPosition -= 16;
-            currentPiece = gameBoard.getState(chainPosition);
-        };
-    };
-
-    // Positive diagonal win condition
-    const posDiagWin = () => {
-        let chainPosition = position;
-        let chainLength = 0;
-
-        // Check upwards (-14)
-        while (chainPosition > 0 && (chainPosition) % 15 != 0 && gameBoard.getState(chainPosition) == piece){
-            chainLength += 1;
-            chainPosition -= 14;
-        };
-
-        // Check downwards (+14)
-        chainPosition = position;
-        currentPiece = piece;
-        while (chainPosition < 225 && (chainPosition + 1) % 15 != 0 && currentPiece == piece){
-            chainLength += 1;
-            chainPosition -= 14;
-            currentPiece = gameBoard.getState(chainPosition);
-        };
-    };
-
-    return {horizontalWin, posDiagWin, negDiagWin, verticalWin};
-
-})();
-
-//// Game logic
-const gameController = (() => {
-    const player1 = Player("x");
-    const player2 = Player("y");
+// Gameplay
+const game = (() => {
+    const player1 = Player('X');
+    const player2 = Player('O');
     let currPlayer = player1;
-    let lastMove = None;
 
-    // Place a piece down
-    const makeMove = (currPlayer, index) => {
-        // Check //TODO: Is it a legitimate move?
-        if(rules.freeSpaceRule(index, currPlayer.getPiece())){
-            gameBoard.setState(index, currPlayer.getPiece());
+    const playRound = (position) => {       // Place the piece down
+        gameBoard.setState(position, currPlayer.getPiece());
+        if(currPlayer == player1){
+            currPlayer = player2;
+        } else {
+            currPlayer = player1;
         }
-        lastMove = index;
-        if (currPlayer == player1){
-            
-        };
     };
 
-    const checkWin = (index)
-
-    // New game
-
-    // End the game
-
+    return {playRound};
 })();
 
 
 
-//// Widget logic
+//// DOM logic
+const displayController = (() => {
+    const fieldElements = document.querySelectorAll(".field");  // Select all fields  
+    const resetButton = document.getElementById("reset-button");
+    const forfeitButton = document.getElementById("forfeit-button");
 
-// Place a piece
+    fieldElements.forEach((field) =>
+    field.addEventListener("click", (e) => {
+        game.playRound();
+        e.target.textContent = gameBoard.getLastPiece();
+
+    })
+    );
+
+    resetButton.addEventListener("click", () => {
+        for(i = 0; i < fieldElements.length; i++){
+            fieldElements[i].textContent = "";
+        }
+    });
+
+    return {updateBoard};
+});
+
+displayController();
